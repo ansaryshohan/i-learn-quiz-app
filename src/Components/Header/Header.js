096683e6c39afe1aa1f86df1702cbe1bafb5ed78
ansaryshohan <ansarye2083@gmaail.com>
2022-11-27 01:14:54 +0600

    router setup completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,26 +7,26 @@ const Header = () => {
   return (
     <div>
 
-      <div class="navbar-comp">
-        <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-          <div class="relative flex items-center justify-between">
+      <div className="navbar-comp">
+        <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+          <div className="relative flex items-center justify-between">
             <NavLink
               to="/"
               aria-label="iLearn"
               title="iLearn"
-              class="inline-flex items-center"
+              className="inline-flex items-center"
             >
-              <span class="ml-2 text-2xl font-bold tracking-wide text-white">
+              <span className="ml-2 text-2xl font-bold tracking-wide text-white">
                 <span>i</span>Learn
               </span>
             </NavLink>
-            <ul class="flex items-center hidden space-x-8 lg:flex">
+            <ul className="flex items-center hidden space-x-8 lg:flex">
               <li>
                 <NavLink
                   to="/topics"
                   aria-label="Topics"
                   title="Topics"
-                  class="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
+                  className="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
                 >
                   Topics
                 </NavLink>
@@ -36,7 +36,7 @@ const Header = () => {
                   to="/statistics"
                   aria-label="Statistics"
                   title="Statistics"
-                  class="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
+                  className="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
                 >
                   Statistics
                 </NavLink>
@@ -46,30 +46,30 @@ const Header = () => {
                   to="/blog"
                   aria-label="Blog"
                   title="Blog"
-                  class="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
+                  className="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
                 >
                   Blog
                 </NavLink>
               </li>
               <li>
                 <NavLink
-                  href="/about"
+                  to="/about"
                   aria-label="About us"
                   title="About us"
-                  class="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
+                  className="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
                 >
                   About us
                 </NavLink>
               </li>
             </ul>
-            <div class="lg:hidden">
+            <div className="lg:hidden">
               <button
                 aria-label="Open Menu"
                 title="Open Menu"
-                class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
                 onClick={() => setIsMenuOpen(true)}
               >
-                <svg class="w-5 text-white" viewBox="0 0 24 24">
+                <svg className="w-5 text-white" viewBox="0 0 24 24">
                   <path
                     fill="currentColor"
                     d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -85,17 +85,17 @@ const Header = () => {
                 </svg>
               </button>
               {isMenuOpen && (
-                <div class="absolute top-0 left-0 w-full">
-                  <div class="p-5 navbar-comp border rounded shadow-sm">
-                    <div class="flex items-center justify-between mb-4">
+                <div className="absolute top-0 left-0 w-full">
+                  <div className="p-5 navbar-comp border rounded shadow-sm">
+                    <div className="flex items-center justify-between mb-4">
                       <div>
                         <NavLink
                           to="/"
                           aria-label="iLearn"
                           title="iLearn"
-                          class="inline-flex items-center"
+                          className="inline-flex items-center"
                         >
-                          <span class="ml-2 text-2xl font-bold tracking-wide text-white">
+                          <span className="ml-2 text-2xl font-bold tracking-wide text-white">
                             <span>i</span>Learn
                           </span>
                         </NavLink>
@@ -104,10 +104,10 @@ const Header = () => {
                         <button
                           aria-label="Close Menu"
                           title="Close Menu"
-                          class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                          className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                           onClick={() => setIsMenuOpen(false)}
                         >
-                          <svg class="w-5 text-white" viewBox="0 0 24 24">
+                          <svg className="w-5 text-white" viewBox="0 0 24 24">
                             <path
                               fill="currentColor"
                               d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -117,13 +117,13 @@ const Header = () => {
                       </div>
                     </div>
                     <nav>
-                      <ul class="space-y-4">
+                      <ul className="space-y-4">
                         <li>
                           <NavLink
                             to="/topics"
                             aria-label="Topics"
                             title="Topics"
-                            class="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
+                            className="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
                           >
                             Topics
                           </NavLink>
@@ -133,7 +133,7 @@ const Header = () => {
                             to="/statistics"
                             aria-label="Statistics"
                             title="Statistics"
-                            class="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
+                            className="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
                           >
                             Statistics
                           </NavLink>
@@ -143,17 +143,17 @@ const Header = () => {
                             to="/blog"
                             aria-label="Blog"
                             title="Blog"
-                            class="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
+                            className="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
                           >
                             Blog
                           </NavLink>
                         </li>
                         <li>
                           <NavLink
-                            href="/about"
+                            to="/about"
                             aria-label="About us"
                             title="About us"
-                            class="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
+                            className="font-medium text-xl tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"
                           >
                             About us
                           </NavLink>
--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+import About from "../Components/About/About";
+import Blog from "../Components/Blog/Blog";
 import ErrorPage from "../Components/ErrorPage/ErrorPage";
+import Statistics from "../Components/Statistics/Statistics";
 import Topics from "../Components/Topics/Topics";
 import Main from "../Layouts/Main";
 
@@ -10,9 +13,25 @@ export const router= createBrowserRouter([
    element:<Main></Main>,
    children:([
     {
+      path:'/',
+      element: <Topics></Topics>,
+    },
+    {
       path:'topics',
       element: <Topics></Topics>,
-    }
+    },
+    {
+      path:'statistics',
+      element: <Statistics></Statistics>,
+    },
+    {
+      path:'blog',
+      element: <Blog></Blog>,
+    },
+    {
+      path:'about',
+      element: <About></About>,
+    },
    ]),
    
   },
